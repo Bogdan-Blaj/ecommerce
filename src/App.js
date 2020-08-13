43fd5ffe144098f897d572c19a87b2dc91174775
Bogdan-Blaj <blajbogdan94@gmail.com>
2020-08-14 00:42:44 +0300

    Added Router, sending link for routing and data from array to the menu component to be displayed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import {Switch, Route} from 'react-router-dom';
+
+
+const ComponentA = props => {
+  return (
+    <div>
+        <h1> Data Receive from props matches link : {props.match.params.topicId }</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <HomePage />
+    
+    <div>
+      <Switch>
+        <Route exact path ='/' component={HomePage} />
+        <Route exact path ='/componentA/:topicId' component={ComponentA} />
+      </Switch>
+    </div>
   );
 }
 
