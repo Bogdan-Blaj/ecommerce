c0b748fd67eeee5f8832c17a1196bf5a3f2eed31
Bogdan-Blaj <blajbogdan94@gmail.com>
2020-08-16 01:33:55 +0300

    added collection preview

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import {Switch, Route} from 'react-router-dom';
-
-
-const ComponentA = props => {
-  return (
-    <div>
-        <h1> Data Receive from props matches link : {props.match.params.topicId }</h1>
-    </div>
-  );
-}
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
-    
     <div>
       <Switch>
         <Route exact path ='/' component={HomePage} />
-        <Route exact path ='/componentA/:topicId' component={ComponentA} />
+        <Route exact path ='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
